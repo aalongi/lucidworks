edf1ea69f78b0c1fd1df205c1483c5701ecec4ed
Ashley Alongi <ashley.alongi@bmcjax.com>
2018-02-08 18:28:03 -0500

    implementing pagination

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,24 @@ var itunesApp = angular.module('itunesApp', []);
 itunesApp.controller('SearchController', function SearchController($scope, $http) {
 	$http.get("https://itunes.apple.com/search?term=goo+goo+dolls&kind=song&wrapperType=track").then(function(response) {
 			console.log(response);
-			$scope.itunes = response.data;
+			
+			// function creating page variables for pagination
+			var showData = function() {
+				$scope.curPage = 0;
+				$scope.pageSize = 5;
+				$scope.itunes = response.data;
+				$scope.pagesMath = Math.ceil($scope.itunes.results.length / $scope.pageSize);
+				//console.log(Math.ceil($scope.itunes.results.length / $scope.pageSize));
+				};
+			showData();
 		});
+});
+
+// module for the pagination
+angular.module('itunesApp').filter('pagination', function() {
+	return function(input, start) {
+		start = +start;
+		//console.log(start)
+		return input.slice(start);
+	};
 });
